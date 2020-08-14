eb0dfe749efff918e75ca3deb662b56266d01af1
Sergei <hovorovskyi@gmail.com>
2020-08-14 09:39:31 +0300

    feature/module-4-06-own-events: create onDeleted event

--- 1_todo/10-module-4/src/components/todo-list-item/todo-list-item.js
+++ 1_todo/10-module-4/src/components/todo-list-item/todo-list-item.js
@@ -50,7 +50,8 @@ export default class TodoListItem extends Component {
 
 				<button
 					type="button"
-					className="btn btn-outline-danger btn-sm float-right"
+          className="btn btn-outline-danger btn-sm float-right"
+          onClick={this.props.onDeleted}
 				>
 					<i className="fa fa-trash-o" />
 				</button>
